c54187395da5f6a3b16b4f73118c894cc57841f5
Gloire Mutaliko <mufunyig@gmail.com>
2026-03-16 16:37:03 +0200

    feat: routing and forwarding requests

--- packages/plugin-plane/src/index.ts
+++ packages/plugin-plane/src/index.ts
@@ -2,3 +2,4 @@ export { bootstrap } from './main';
 export { PlaneProxyModule } from './plane-proxy.module';
 export { PlanePluginOptions, PlanePluginAsyncOptions, PLANE_PLUGIN_OPTIONS } from './plane-plugin-options.interface';
 export { PlaneConfigRegistry } from './plane-config.registry';
+export { mountPlaneProxy, MountPlaneProxyOptions, MountPlaneProxyResult } from './mount';
